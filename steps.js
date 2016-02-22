4ee21e4701f2f172091165439498ee4ed54fae28
Dirk Grappendorf <dirk@grappendorf.net>
2016-02-22 22:27:19 +0100

    feat: fill in form field step

--- steps.js
+++ steps.js
@@ -16,6 +16,21 @@ module.exports = function() {
     });
   });
 
+  this.When(/^I fill in "([^"]*)" with "([^"]*)"$/, function(label, text, next) {
+    var self = this;
+    self.driver.findElement(self.webdriver.By.xpath("//label[normalize-space()='" + label + "']")).then(function(element) {
+      element.getAttribute('id').then(function(id) {
+        self.driver.findElement(self.webdriver.By.xpath("//input[@aria-labelledby='" + id + "']")).then(function(element) {
+          element.sendKeys(text).then(function() {
+            next();
+          });
+        })
+      });
+    }, function() {
+      next.fail('Page doesn\'t contain "' + label + '"');
+    });
+  });
+
   this.Then(/^I should see "([^"]*)"$/, function(text, next) {
     this.driver.getPageSource().then(function(page) {
       if (page.indexOf(text) >= 0) {
